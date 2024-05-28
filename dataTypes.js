275d835eace2e9603cff91f47af3ff20bbeac043
shivpatel027 <patelshiv1701@gmail.com>
2024-05-27 20:39:35 -0500

    Data types added

---
+++ dataTypes.js
@@ -0,0 +1,42 @@
+// Dynamic typing
+
+// let age = 25;
+// age = 'Shiv';
+// console.log(age); 
+
+// 1. Primitive data types
+// 1. Number
+// let temp = -25;
+// let price = 25.25;
+ 
+// 2. String
+// let name = 'Shiv';
+
+// 3. Boolean
+// true/false
+// let isLoggedIn = ture;
+// let hasPermission = false;
+
+// 4. Undefined
+// let age;
+// console.log(age);
+
+// 5. BigInt
+// 6. Symbol
+    // Unique
+
+
+// 2. Structural type
+// 1. Object
+
+    // 1.1 Function
+    // Non-data structure
+    // callable
+
+    // 1.2 Arrays
+    // collection of values
+    //const number = [2, 3, 4. 'javascript', 'python', {name: 'Shiv}]; 
+
+    // 1.3 Maps
+
+    // 1.4 Set 
