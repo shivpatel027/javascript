023adfedae29f4932b31e86c0436d26cae14629a
shivpatel027 <patelshiv1701@gmail.com>
2024-05-28 05:15:13 -0500

    operators added

---
+++ operators.js
@@ -0,0 +1,113 @@
+// Operators
+
+// 1. Assignment Operator
+
+// let x = 10;
+// let y;
+// y = x;
+// console.log(y);
+
+// Compound operators
+
+// let x = 10;
+// let y = 20;
+
+// x += y;
+// x -= y;
+// x /= y;
+// x *= y;
+
+// console.log(x); 
+
+// Arithmetic operators
+// + 
+// -
+// *
+// /
+// ** (eponantial operator - used for power of the number);
+
+// let x = 10;
+// console.log(x ** 3);
+
+// let x = 10;
+// x++;
+// x--;
+// console.log(x);
+
+// Logical operators - it evaluates from left to right.
+// 1. Logical AND &&
+// let happy = true && true; // true
+// let happy = true && false; // false
+// let happy = false && true; // false
+// let happy = false && false; // false
+
+// console.log(happy);
+
+// const isLoggedIn = true;
+// const hasPermission = true;
+
+// if(isLoggedIn && hasPermission) {
+//     console.log('user is logged in and has permission');
+// }
+
+// 2. Logical OR ||
+// let happy = true || true; // true
+// let happy = true || false; // true
+// let happy = false || true; // true
+// let happy = false || false; // false
+
+// console.log(happy);
+
+// const isLoggedIn = true;
+// const hasPermission = true;
+
+// if(isLoggedIn || hasPermission) {
+//     console.log('user is logged in and has permission');
+// }
+
+// 3. Logical NOT !
+
+// const happy = true;
+// console.log(!happy); // prints false
+
+// const animal = 'Cat';
+// console.log(!animal); // prints false;
+
+// const isLoggedIn = false;
+// const hasPermission = true;
+
+// if(!isLoggedIn) { // if you want to neggate the condition
+//     console.log('user is logged in and has permission');
+// }
+
+
+// Conditional Operatoes (Ternary operators)
+
+const userRole = 'admin';
+
+// if(userRole === 'admin') {
+//     console.log('You are an admin');
+// } else {
+//     console.log('You are not an admin');
+// }
+
+// userRole === 'admin' ? console.log('You are an admin') : console.log('You are not an admin');
+
+// Comparison operator
+// Equal (==)
+
+// Not equal (!=)
+
+// Strict equal (===)
+//coearcen - it does not convert from one type to another type (others do means ==)
+// use === for good practices
+
+// Strict not equal (!==)
+
+// Greater than (>)
+
+// Greater than or equal (>=)
+
+// Less than(<)
+
+// Less than or equal(<=)
