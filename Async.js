eb9679c9fcd090adf731ea346b0b04c4f937613b
shivpatel027 <patelshiv1701@gmail.com>
2024-05-30 06:30:40 -0500

    Asynxronus JS added

---
+++ Async.js
@@ -0,0 +1,14 @@
+// Asyncronous Javascript
+// console.log('hello');
+// function greetings() {
+//     console.log('I am from setTimeout');
+// }
+// setTimeout(greetings, 3000)
+// console.log('javascript');
+
+
+console.log('start');
+setTimeout(() => {
+    console.log('I am from setTimeout');
+},0); 
+console.log('end');
