b3a04f45e472de43cc77865c222baf024ca38966
shivpatel027 <patelshiv1701@gmail.com>
2024-05-29 22:24:47 -0500

    function started

---
+++ functions.js
@@ -0,0 +1,59 @@
+// function login(username, password) {
+    // code
+    // logic
+//     console.log(password);
+//     console.log(`${username}, loggedin successfully`);
+// }
+
+// login('shiv', 'likewise');
+// login('jeel');
+
+// function uppercase(str) {
+//     return str.toUpperCase();
+// }
+
+//const result = uppercase('shiv');
+//console.log(result);
+
+// default perameter
+// function calculateArea(width, height = 1) {
+//     const area = width * height;
+//     return area;
+// }
+
+// const result = calculateArea(30, 40);
+// console.log(result);
+
+// variable scope
+// const fileName = 'xyz.pdf';
+// function download() {
+    // download logic here
+//     console.log(fileName);
+
+// }
+// download();
+
+// Declaration
+// function logic () {
+
+
+// }
+
+// Expression
+// const login = function (a, b) {
+//     console.log('loggd in');
+// }
+
+// login();
+
+// callback
+// function formatText(text, formatCb) {
+//     return typeof text === 'function' ? formatCb(text) : text.toUpperCase();
+// }
+
+// const result = formatText('shiv', function(text) {
+//     text.charAt(0).toUpperCase() + text.slice(1);
+// });
+// console.log(result);
+
+// IIFE ( Immediately invoked function expression)
